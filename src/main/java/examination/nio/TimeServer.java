81df56e67c4583c11c9ebf18928a1d2a7b26b85d
xiaoguoqiang <jannlee68@163.com>
2017-09-20 21:52:23 +0800

    remove files

---
+++ src/main/java/examination/nio/TimeServer.java
@@ -0,0 +1,21 @@
+package examination.nio;
+
+public class TimeServer {
+	
+	public static void main(String[] args){
+		int port = 9527;
+		
+		if(args != null && args.length >0){
+			try{
+				port = Integer.valueOf(args[0]);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		MultiplexerTimeServer timeServer =  new MultiplexerTimeServer(port);
+		new Thread(timeServer,"NIO-MultiplexerTimeServer-001").start();
+		
+	}
+
+}
