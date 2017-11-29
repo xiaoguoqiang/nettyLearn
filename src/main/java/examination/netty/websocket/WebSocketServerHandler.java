c0959a51b08d40cbed4b33fe18ce292b05e72c73
xiaoguoqiang <jannlee68@163.com>
2017-11-29 22:30:34 +0800

    complete websocketserverHandler

--- src/main/java/examination/netty/websocket/WebSocketServerHandler.java
+++ src/main/java/examination/netty/websocket/WebSocketServerHandler.java
@@ -1,13 +1,109 @@
 package examination.netty.websocket;
 
+import org.apache.log4j.Logger;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
 
-public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object>{
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+
+	private static final Logger logger = Logger.getLogger(WebSocketServerHandler.class.getName());
+
+	private WebSocketServerHandshaker handshaker;
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-		
+
+		if (msg instanceof FullHttpRequest) {
+			handleHttpRequest(ctx, (FullHttpRequest) msg);
+		} else if (msg instanceof WebSocketFrame) {
+			handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+		}
+
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.flush();
+	}
+
+	private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest msg) {
+		if (!msg.decoderResult().isSuccess() || !"websocket".equals(msg.headers().get("Upgrade"))) {
+			sendResponse(ctx, msg, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+		}
+
+		WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
+				"ws://localhost:8080/websocket", null, false);
+		handshaker = wsFactory.newHandshaker(msg);
+		if (handshaker == null) {
+			WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+		} else {
+			//自动切换pipeline中的handler
+			handshaker.handshake(ctx.channel(), msg);
+		}
+
+	}
+
+	private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+		if (frame instanceof CloseWebSocketFrame) {
+			handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+			return;
+		}
+		if (frame instanceof PingWebSocketFrame) {
+			ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+			return;
+		}
+		if (!(frame instanceof TextWebSocketFrame)) {
+			throw new UnsupportedOperationException(
+					String.format("%s frame type not supported", frame.getClass().getName()));
+		}
+
+		String request = ((TextWebSocketFrame) frame).text();
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("%s received %s", ctx.channel(), request));
+		}
+		ctx.channel().write(
+				new TextWebSocketFrame(request + ",欢迎使用netty websocket服务，现在时刻：" + new java.util.Date().toString()));
+
+	}
+
+	private void sendResponse(ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse rsp) {
+		if (rsp.status().code() != 200) {
+			ByteBuf buf = Unpooled.copiedBuffer(rsp.status().toString(), CharsetUtil.UTF_8);
+			rsp.content().writeBytes(buf);
+			buf.release();
+			HttpUtil.setContentLength(rsp, rsp.content().readableBytes());
+		}
+
+		ChannelFuture cf = ctx.channel().writeAndFlush(rsp);
+		if (!HttpUtil.isKeepAlive(req) || rsp.status().code() != 200) {
+			cf.addListener(ChannelFutureListener.CLOSE);
+		}
+
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
 	}
 
 }
